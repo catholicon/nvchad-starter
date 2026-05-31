4b15b852887e7dd84fd75aca2fa20a2e33babec5
Vikas Saurabh <vsaurabh@adobe.com>
2026-05-31 08:19:49 +0530

    bring C-d and C-u from old setup

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Scroll with cursor centered
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down (centered)" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up (centered)" })
+
 -- mapping augment commands
 map({"n", "v"}, "<leader>ac", ":Augment chat<CR>")
 map("n", "<leader>an", ":Augment chat-new<CR>")
