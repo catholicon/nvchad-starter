c98172da6e5e02f920b2eb274a643fef3ac04ecd
Vikas Saurabh <vsaurabh@adobe.com>
2026-05-31 08:23:52 +0530

    setup treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,13 +22,30 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- nvchad defaults
+        "lua", "luadoc", "printf", "vim", "vimdoc",
+        -- web
+        "html", "css", "javascript", "typescript", "tsx",
+        -- systems
+        "c", "cpp", "go", "rust",
+        -- scripting / config
+        "bash", "json", "yaml",
+        -- docs
+        "markdown", "markdown_inline",
+      },
+    },
+    init = function()
+      -- Highlighting is handled by NvChad's FileType autocmd (pcall vim.treesitter.start)
+      -- This adds treesitter-based indentation on top
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
 }
