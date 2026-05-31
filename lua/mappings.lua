4c43795b0d7b932f580d4e084c706a74cbbc7f27
Vikas Saurabh <vsaurabh@adobe.com>
2026-06-01 00:01:35 +0530

    enabled <leader>ca for code actions

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
 map("i", "jk", "<ESC>")
 
 -- Scroll with cursor centered
