18d6986d1d50d27bf295271e24ef878a15cad99e
Vikas Saurabh <vsaurabh@adobe.com>
2026-09-15 14:02:59 +0530

    enable blame line on current line

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -266,4 +266,12 @@ return {
       })
     end,
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup {
+        current_line_blame = true,
+      }
+    end
+  },
 }
